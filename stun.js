4593baafaff24b7c666bff8b170e69ca59b489bb
kimsimon75 <be555@naver.com>
2025-03-19 03:34:25 +0900

    Auto commit: 2025-03-19 03:34:25

--- stun.js
+++ stun.js
@@ -1,4 +1,15 @@
+const webSocket = new WebSocket("ws://localhost:8005");
 
+         // 서버에서 wss.on('connection' 이 성공적으로 되면, 이벤트 실행
+         webSocket.onopen = function () {
+            console.log('서버와 웹소켓 연결 성공!');
+         };
+
+         // 사실상 .onmessage 와 .send 로 메세지 통신을 하게 되는 것이다
+         webSocket.onmessage = function (event) {
+            console.log(event.data);
+            webSocket.send('클라이언트에서 서버로 답장을 보냅니다');
+            
 const unitRates = {
     특별함: ["특별함",0],
     희귀함: ["희귀함", 1],
