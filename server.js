cfca2caee63ba5b4d5992a8d65d64ed553614609
kimsimon75 <be555@naver.com>
2025-03-19 03:05:36 +0900

    Auto commit: 2025-03-19 03:05:36

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.use('/Stun.css', express.static(__dirname + '/Stun.css'));
+app.use('/stun.js', express.static(__dirname + '/stun.js'));
+app.use('/plus.png', express.static(__dirname + '/plus.png'));
+app.use('/minus.png', express.static(__dirname + '/minus.png'));
 
 app.listen(8080, function(){
     console.log('listening on 8080');
