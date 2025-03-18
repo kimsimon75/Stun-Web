bfd5d1b9c9864643c2ffed8693cf14acd83f1a22
kimsimon75 <be555@naver.com>
2025-03-19 03:29:16 +0900

    Auto commit: 2025-03-19 03:29:15

--- server.js
+++ server.js
@@ -1,11 +1,4 @@
 const express = require('express');
-const path = require('path');
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const nunjucks = require('nunjucks');
-const dotenv = require('dotenv');
-
 const app = express();
 
 app.use('/Stun.css', express.static(__dirname + '/Stun.css'));
