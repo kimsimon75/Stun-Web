449b352b136f02d059f91171134678bdae1feebe
kimsimon75 <be555@naver.com>
2025-03-19 03:32:21 +0900

    Auto commit: 2025-03-19 03:32:21

--- server.js
+++ server.js
@@ -13,9 +13,3 @@ app.listen(8080, function(){
 app.get("/", function(요청, 응답){
     응답.sendFile(__dirname + '/index.html');
 });
-
-const socket = new WebSocket("");
-
-socket.onopen = () =>{
-    console.log("WebSocket is open");
-}
