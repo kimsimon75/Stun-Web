8f6b84584ff4556ac8c7a77e173329688515742d
kimsimon75 <be555@naver.com>
2025-03-19 03:27:11 +0900

    Auto commit: 2025-03-19 03:27:11

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const nunjucks = require('nunjucks');
+const dotenv = require('dotenv');
+
 const app = express();
 
 app.use('/Stun.css', express.static(__dirname + '/Stun.css'));
@@ -14,3 +21,8 @@ app.get("/", function(요청, 응답){
     응답.sendFile(__dirname + '/index.html');
 });
 
+const socket = new WebSocket("");
+
+socket.onopen = () =>{
+    console.log("WebSocket is open");
+}
