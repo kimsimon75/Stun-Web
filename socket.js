2b2eab7e236bf218408c90cc4df38b13da76bb9b
kimsimon75 <be555@naver.com>
2025-03-19 03:47:47 +0900

    Auto commit: 2025-03-19 03:47:47

--- socket.js
+++ socket.js
@@ -1,41 +1,20 @@
 const WebSocket = require('ws');
 
-module.exports = server => {
-   //? express 서버와 웹소켓 서버를 연결 시킨다.
-   // 변수이름은 wss(web socket server)
-   const wss = new WebSocket.Server({ server }); 
+const server = new WebSocket.Server({ port: 8080 });
 
-   //* 프론트에서 new WebSocket("ws://localhost:8005") 보냈을때, 웹소켓 연결 실행
-   wss.on('connection', (ws, req) => {
-      //* ip 파악
-      // req.headers['x-forwarded-for'] : 프록시 서버를 경유할때 ip가 변조될수 있다. 이를 감지하고 본ip파악해줄 수 있다.
-      const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-      console.log('새로운 클라이언트 접속 ip : ', ip);
+server.on('connection', (ws) => {
+    console.log('새로운 클라이언트 연결됨!');
 
-      //* 클라이언트로부터 온 메시지
-      ws.on('message', message => {
-         console.log(message.toString());
-      });
+    ws.send('서버: 연결 성공!');
 
-      //* 에러 시
-      ws.on('error', error => {
-         console.error(error);
-      });
+    ws.on('message', (message) => {
+        console.log(`클라이언트로부터 받은 메시지: ${message}`);
+        ws.send(`서버에서 받은 메시지: ${message}`);
+    });
 
-      //* 연결 종료 시 (유저가 채팅방을 나간다거나)
-      ws.on('close', () => {
-         console.log('클라이언트 접속 해제', ip);
-         clearInterval(ws.interval); // 연결 끊기면 setInterval 중지
-      });
+    ws.on('close', () => {
+        console.log('클라이언트 연결 종료');
+    });
+});
 
-      // 3초마다 클라이언트로 메시지 전송
-      ws.interval = setInterval(() => {
-         //! 웹소켓은 비동기이기 때문에 삑 날 수 있어, 웹소켓이 클라이언트랑 연결이 되었는지 검사하는 안전 장치
-         if (ws.readyState !== ws.OPEN) {
-            return;
-         }
-
-         ws.send('서버에서 클라이언트로 메시지를 보냅니다.');
-      }, 3000);
-   });
-};
+console.log('웹소켓 서버가 ws://localhost:8080 에서 실행 중!');
