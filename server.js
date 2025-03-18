9a77cbf0d4ea991fda7e3d4fb19986634808f06e
kimsimon75 <be555@naver.com>
2025-03-19 04:14:35 +0900

    Auto commit: 2025-03-19 04:14:35

--- server.js
+++ server.js
@@ -2,11 +2,9 @@ const WebSocket = require('ws');
 
 const server = new WebSocket.Server({ port: 8080 });
 
-server.on('connection', (ws, req) => {
+server.on('connection', (ws) => {
     console.log('✅ 클라이언트가 연결되었습니다.');
-
-    ws.send('서버: 연결 성공!');
-
+    
     ws.on('message', (message) => {
         console.log(`📩 받은 메시지: ${message}`);
         ws.send(`서버에서 받은 메시지: ${message}`);
