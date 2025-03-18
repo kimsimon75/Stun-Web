b20b370ef8b705a149656b2365ab0c38888c7d06
kimsimon75 <be555@naver.com>
2025-03-19 03:55:00 +0900

    Auto commit: 2025-03-19 03:55:00

--- server.js
+++ server.js
@@ -13,20 +13,3 @@ app.listen(8080, function(){
 app.get("/", function(요청, 응답){
     응답.sendFile(__dirname + '/index.html');
 });
-
-const ws = new WebSocket('ws://localhost:8080');
-
-ws.onopen = () => {
-    console.log('서버와 웹소켓 연결 완료!');
-    document.getElementById('output').innerText = "서버와 연결되었습니다!";
-};
-
-ws.onmessage = (event) => {
-    console.log(`서버 응답: ${event.data}`);
-    document.getElementById('output').innerText = `서버 응답: ${event.data}`;
-};
-
-function sendMessage() {
-    const msg = document.getElementById('message').value;
-    ws.send(msg);
-}
