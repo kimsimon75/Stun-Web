91cb236515e03fab7d183fc038ee35fc29d245a1
kimsimon75 <kimsimon75@gmail.com>
2025-03-19 22:38:15 +0900

    Auto commit: 2025-03-19 22:38:15

--- stun.js
+++ stun.js
@@ -1,7 +1,17 @@
+const socket = new WebSocket("wss://o5wmuffu1h.execute-api.ap-southeast-2.amazonaws.com/sendMessage/");
 
-    alert(hello);  // 알림 띄우기
+socket.onopen = () => {
+  console.log("✅ 웹소켓 연결 성공!");
+};
+
+socket.onmessage = (event) => {
+  const message = JSON.parse(event.data);
+  if (message.action === "webhook") {
+    alert(message.data);  // 알림 띄우기
     location.reload();     // 새로고침
-  
+  }
+};
+
 socket.onclose = () => {
   console.log("❌ 웹소켓 연결 종료됨");
 };
