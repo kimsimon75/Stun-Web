20c8dd03f430b65dff7410af4ec74ff47a697464
kimsimon75 <kimsimon75@gmail.com>
2025-03-19 23:11:09 +0900

    Auto commit: 2025-03-19 23:11:09

--- stun.js
+++ stun.js
@@ -1,4 +1,26 @@
+const socket = new WebSocket("wss://o5wmuffu1h.execute-api.ap-southeast-2.amazonaws.com/sendMessage/");
 
+socket.onopen = (event) => {
+  console.log("✅ 웹소켓 연결 성공!"); 
+   const message = JSON.parse(event.data);
+  if (message.action === "webhook") {
+    alert(message.data);  // 알림 띄우기
+    location.reload();     // 새로고침
+  }
+};
+
+socket.onmessage = (event) => {
+  const message = JSON.parse(event.data);
+  if (message.action === "webhook") {
+    alert(message.data);  // 알림 띄우기
+    location.reload();     // 새로고침
+  }
+};
+
+socket.onclose = () => {
+  console.log("❌ 웹소켓 연결 종료됨");
+};
+const char = 1;
 const unitRates = {
     특별함: ["특별함",0],
     희귀함: ["희귀함", 1],
