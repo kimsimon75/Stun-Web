97614ffd3011b1653ad7ae97e62b54c394c461c1
kimsimon75 <kimsimon75@gmail.com>
2025-03-20 08:04:12 +0900

    Auto commit: 2025-03-20 08:04:12

--- stun.js
+++ stun.js
@@ -1,26 +1,4 @@
-const socket = new WebSocket("wss://o5wmuffu1h.execute-api.ap-southeast-2.amazonaws.com/production");
 
-socket.onopen = () => {
-  console.log("✅ 웹소켓 연결 성공!");
-};
-
-socket.onmessage = (event) => {
-    console.log("hello");
-  const message = JSON.parse(event.data);
-  if (message.action === "webhook") {
-    alert(message.data); // 알림 띄우기
-    location.reload(); // 새로고침
-  }
-};
-const message = JSON.stringify({
-    action: "connect",  // API Gateway에서 설정한 라우트 이름
-    data: "Hello, WebSocket!"
-});
-
-
-socket.onclose = () => {
-  console.log("❌ 웹소켓 연결 종료됨");
-};
 const char = 1;
 const unitRates = {
     특별함: ["특별함",0],
@@ -1907,3 +1885,26 @@ MoveSpeedPage.addEventListener('click', () => {
 
 document.getElementsByClassName(`Stack`)[0].appendChild(MoveSpeedPage);
 
+const socket = new WebSocket("wss://o5wmuffu1h.execute-api.ap-southeast-2.amazonaws.com/production");
+
+socket.onopen = () => {
+  console.log("✅ 웹소켓 연결 성공!");
+};
+
+socket.onmessage = (event) => {
+    console.log("hello");
+  const message = JSON.parse(event.data);
+  if (message.action === "webhook") {
+    alert(message.data); // 알림 띄우기
+    location.reload(); // 새로고침
+  }
+};
+const message = JSON.stringify({
+    action: "connect",  // API Gateway에서 설정한 라우트 이름
+    data: "Hello, WebSocket!"
+});
+
+
+socket.onclose = () => {
+  console.log("❌ 웹소켓 연결 종료됨");
+};
