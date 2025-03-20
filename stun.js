7659b39cfa51684243f1f1f56fa987e28e038bd9
kimsimon75 <kimsimon75@gmail.com>
2025-03-20 15:22:39 +0900

    Auto commit: 2025-03-20 15:22:39

--- stun.js
+++ stun.js
@@ -1899,12 +1899,12 @@ socket.onopen = () => {
 };
 
 socket.onmessage = (event) => {
+    console.log("📩 서버로부터 메시지 수신:", event.data);
     const message = JSON.stringify({
         action: "sendMessage",  // API Gateway에서 설정한 라우트
         data: "Hello, WebSocket!"
     });
     socket.send(message);
-    console.log("📩 서버로부터 메시지 수신:", event.data);
 };
 
 socket.onerror = (error) => {
