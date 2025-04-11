62baaf61708cb024ce442ea95b243fa139dcfbb6
kimsimon75 <kimsimon75@gmail.com>
2025-04-12 08:46:29 +0900

    Auto commit: 2025-04-12 08:46:29

--- stun.js
+++ stun.js
@@ -2841,7 +2841,7 @@ function connectWebSocket(){
     
         if(JSON.parse(event.data).id === "lastUpdate")
         {
-            console.log(JSON.parse(event.data));
+            console.log(JSON.parse(event.data).updatedAt);
             showUpdateNotification();
         }
     };
